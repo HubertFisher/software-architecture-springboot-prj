4bd5ee37e2f5b44d8d68e56630ff083dedcb0894
Ali Tulegenov <Snailpiegeek@gmail.com>
2026-01-30 16:33:28 +0100

    build: Add core dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,37 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 repositories {
     mavenCentral()
 }
 
+
 dependencies {
+    // =================== SPRING BOOT ===================
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // =================== UTILITIES ===================
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // =================== DATABASE ===================
+    runtimeOnly("org.postgresql:postgresql")
+
+    // =================== TESTING ===================
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
